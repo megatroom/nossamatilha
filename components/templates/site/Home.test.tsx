f38c99fbe06029924e37163928e134039030a1f9
Bruno Nardini <bruno.nardini@rdstation.com>
2022-02-17 10:29:58 -0300

    add test config

---
+++ components/templates/site/Home.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Home from "./Home";
+import Theme from "../../../styles/Theme";
+
+it("should render call to action", () => {
+  render(
+    <Theme>
+      <Home />
+    </Theme>
+  );
+
+  expect(screen.getByText("Saiba mais")).toBeInTheDocument();
+  expect(screen.getByText("Agendar visita")).toBeInTheDocument();
+});
---
+++ stories/organisms/SiteNavbar.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import SiteNavbar from "../../components/organisms/SiteNavbar";
+
+export default {
+  title: "Organisms/SiteNavbar",
+  component: SiteNavbar,
+  argTypes: {},
+} as ComponentMeta<typeof SiteNavbar>;
+
+const Template: ComponentStory<typeof SiteNavbar> = (args) => (
+  <SiteNavbar {...args} />
+);
+
+export const Base = Template.bind({});
+Base.args = {};
